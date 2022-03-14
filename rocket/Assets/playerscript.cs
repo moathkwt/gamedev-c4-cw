dd4874db14e1d064b6c3ef02d56155a145822a87
moathkwt <100304331+moathkwt@users.noreply.github.com>
2022-03-14 13:50:14 +0300

    Audio edit

--- rocket/Assets/playerscript.cs
+++ rocket/Assets/playerscript.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class playerscript : MonoBehaviour
 {
     Rigidbody rocketRB;
+    private AudioSource rocketAudioSource;
+    AudioSource rocketAudiosource;
+
+    [SerializeField] AudioClip mainEngine;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rocketRB = GetComponent<Rigidbody>();
+        rocketAudioSource = GetComponent<AudioSource>();
+
+
     }
 
     // Update is called once per frame
@@ -23,8 +31,17 @@ public class playerscript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rocketRB.AddRelativeForce(Vector3.up * 20);
+            rocketRB.AddRelativeForce(Vector3.up * 15);
+            if (!rocketAudioSource.isPlaying)
+            {
+                rocketAudioSource.PlayOneShot(mainEngine);
+            }
         }
+        else
+        {
+            rocketAudioSource.Stop();
+        }
+    
 
     }
     void Rotate()
@@ -32,12 +49,12 @@ public class playerscript : MonoBehaviour
         
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward / 2);
         }
         else
             if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward / 2);
         }
         
     }
